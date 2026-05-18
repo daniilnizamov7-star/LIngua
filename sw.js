5b6d1c58e47715e5ee6c24ed54959e70c5c79f3b
daniilnizamov7-star <daniilnizamov7@gmail.com>
2026-05-18 14:22:16 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,39 @@
 const VERSION = 'arabic-v22';
 const ASSETS = ['/', '/index.html', '/manifest.json'];
 
+const VERSION = 'lingua-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json'];
+
 self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(VERSION)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(k => k !== VERSION).map(k => caches.delete(k))
+      ))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Network-first: всегда берём свежую версию, кэш только как запасной офлайн
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(VERSION).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});', e => {
   e.waitUntil(
     caches.open(VERSION)
       .then(c => c.addAll(ASSETS))
